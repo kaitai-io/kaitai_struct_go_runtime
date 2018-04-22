8ecbcbeb2e0070b44b646dc84734e6835ee653ec
Jonas Plum <cugu@users.noreply.github.com>
2018-04-23 00:11:53 +0200

    Update lint.sh

--- tests/lint.sh
+++ tests/lint.sh
@@ -3,6 +3,6 @@ gometalinter.v2 --install
 # runtime
 gometalinter.v2 kaitai
 # tests
-gometalinter.v2 tests/src/spec/
+gometalinter.v2 src/spec/
 # generated code
-gometalinter.v2 tests/src/test_formats/
+gometalinter.v2 src/test_formats/
