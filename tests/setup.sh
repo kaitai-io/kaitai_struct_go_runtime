ef6c0a92a45e6b6b820f19c7884c590a7ddfe382
Jonas Plum <cugu@users.noreply.github.com>
2018-04-23 00:05:51 +0200

    Update setup.sh

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,6 +1,10 @@
 rm -rf src
 mkdir src
 
+mkdir -p $HOME/gopath/src/github.com/github.com/kaitai-io/kaitai_struct_go_runtime
+cp -v -r $HOME/gopath/src/github.com/cugu/kaitai_struct_go_runtime/kaitai $HOME/gopath/src/github.com/kaitai-io/kaitai_struct_go_runtime
+
+
 git clone https://github.com/kaitai-io/kaitai_struct_tests
 mv kaitai_struct_tests/spec/go src/spec
 mv kaitai_struct_tests/src/* src/
@@ -9,8 +13,6 @@ git clone https://github.com/kaitai-io/ci_targets/
 mv ci_targets/compiled/go/src/test_formats src/test_formats
 rm -rf ci_targets
 
-mkdir -p $HOME/gopath/src/github.com/github.com/kaitai-io/
-cp -v -r $HOME/gopath/src/github.com/cugu/kaitai_struct_go_runtime $HOME/gopath/src/github.com/kaitai-io
 
 go get github.com/stretchr/testify/assert
 go get golang.org/x/text
