b0770147234847cfa4532f86028a4c88481605b1
Petr Pucil <petr.pucil@seznam.cz>
2020-09-11 17:51:19 +0200

    Add validation errors (https://github.com/kaitai-io/kaitai_struct/issues/435)

--- kaitai/error.go
+++ kaitai/error.go
@@ -1,5 +1,7 @@
 package kaitai
 
+import "fmt"
+
 // EndOfStreamError is returned when the stream unexpectedly ends.
 type EndOfStreamError struct{}
 
@@ -15,9 +17,183 @@ func (UndecidedEndiannessError) Error() string {
 	return "undecided endianness"
 }
 
-// ValidationNotEqualError is returned when validation fails.
-type ValidationNotEqualError struct{}
+type locationInfo struct {
+	io      *Stream
+	srcPath string
+}
 
-func (ValidationNotEqualError) Error() string {
-	return "validation error"
+func newLocationInfo(io *Stream, srcPath string) locationInfo {
+	return locationInfo{
+		io,
+		srcPath,
+	}
+}
+
+func (l locationInfo) Io() *Stream { return l.io }
+
+func (l locationInfo) SrcPath() string { return l.srcPath }
+
+func (l locationInfo) msgWithLocation(msg string) string {
+	var pos interface{}
+	pos, err := l.io.Pos()
+	if err != nil {
+		pos = "N/A"
+	}
+	return fmt.Sprintf("%s: at pos %v: %s", l.srcPath, pos, msg)
+}
+
+// ValidationFailedError is an interface that all "Validation*Error"s implement.
+type ValidationFailedError interface {
+	Actual() interface{}
+	Io() *Stream
+	SrcPath() string
+}
+
+func validationFailedMsg(msg string) string {
+	return "validation failed: " + msg
+}
+
+// ValidationNotEqualError signals validation failure: we required "Actual" value
+// to be equal to "Expected", but it turned out that it's not.
+type ValidationNotEqualError struct {
+	expected interface{}
+	actual   interface{}
+	locationInfo
+}
+
+// NewValidationNotEqualError creates a new ValidationNotEqualError instance.
+func NewValidationNotEqualError(expected interface{}, actual interface{}, io *Stream, srcPath string) ValidationNotEqualError {
+	return ValidationNotEqualError{
+		expected,
+		actual,
+		newLocationInfo(io, srcPath),
+	}
+}
+
+// Expected is a getter of the expected value associated with the validation error.
+func (e ValidationNotEqualError) Expected() interface{} { return e.expected }
+
+// Actual is a getter of the actual value associated with the validation error.
+func (e ValidationNotEqualError) Actual() interface{} { return e.actual }
+
+func (e ValidationNotEqualError) Error() string {
+	return e.msgWithLocation(
+		validationFailedMsg(
+			fmt.Sprintf("not equal, expected %v, but got %v", e.expected, e.actual),
+		),
+	)
+}
+
+// ValidationLessThanError signals validation failure: we required "Actual" value
+// to be greater than or equal to "Min", but it turned out that it's not.
+type ValidationLessThanError struct {
+	min    interface{}
+	actual interface{}
+	locationInfo
+}
+
+// NewValidationLessThanError creates a new ValidationLessThanError instance.
+func NewValidationLessThanError(min interface{}, actual interface{}, io *Stream, srcPath string) ValidationLessThanError {
+	return ValidationLessThanError{
+		min,
+		actual,
+		newLocationInfo(io, srcPath),
+	}
+}
+
+// Min is a getter of the minimum value associated with the validation error.
+func (e ValidationLessThanError) Min() interface{} { return e.min }
+
+// Actual is a getter of the actual value associated with the validation error.
+func (e ValidationLessThanError) Actual() interface{} { return e.actual }
+
+func (e ValidationLessThanError) Error() string {
+	return e.msgWithLocation(
+		validationFailedMsg(
+			fmt.Sprintf("not in range, min %v, but got %v", e.min, e.actual),
+		),
+	)
+}
+
+// ValidationGreaterThanError signals validation failure: we required "Actual" value
+// to be less than or equal to "Max", but it turned out that it's not.
+type ValidationGreaterThanError struct {
+	max    interface{}
+	actual interface{}
+	locationInfo
+}
+
+// NewValidationGreaterThanError creates a new ValidationGreaterThanError instance.
+func NewValidationGreaterThanError(max interface{}, actual interface{}, io *Stream, srcPath string) ValidationGreaterThanError {
+	return ValidationGreaterThanError{
+		max,
+		actual,
+		newLocationInfo(io, srcPath),
+	}
+}
+
+// Max is a getter of the maximum value associated with the validation error.
+func (e ValidationGreaterThanError) Max() interface{} { return e.max }
+
+// Actual is a getter of the actual value associated with the validation error.
+func (e ValidationGreaterThanError) Actual() interface{} { return e.actual }
+
+func (e ValidationGreaterThanError) Error() string {
+	return e.msgWithLocation(
+		validationFailedMsg(
+			fmt.Sprintf("not in range, max %v, but got %v", e.max, e.actual),
+		),
+	)
+}
+
+// ValidationNotAnyOfError signals validation failure: we required "Actual" value
+// to be from the list, but it turned out that it's not.
+type ValidationNotAnyOfError struct {
+	actual interface{}
+	locationInfo
+}
+
+// NewValidationNotAnyOfError creates a new ValidationNotAnyOfError instance.
+func NewValidationNotAnyOfError(actual interface{}, io *Stream, srcPath string) ValidationNotAnyOfError {
+	return ValidationNotAnyOfError{
+		actual,
+		newLocationInfo(io, srcPath),
+	}
+}
+
+// Actual is a getter of the actual value associated with the validation error.
+func (e ValidationNotAnyOfError) Actual() interface{} { return e.actual }
+
+func (e ValidationNotAnyOfError) Error() string {
+	return e.msgWithLocation(
+		validationFailedMsg(
+			fmt.Sprintf("not any of the list, got %v", e.actual),
+		),
+	)
+}
+
+// ValidationExprError signals validation failure: we required "Actual" value
+// to match the expression, but it turned out that it doesn't.
+type ValidationExprError struct {
+	actual interface{}
+	locationInfo
+}
+
+// NewValidationExprError creates a new ValidationExprError instance.
+func NewValidationExprError(actual interface{}, io *Stream, srcPath string) ValidationExprError {
+	return ValidationExprError{
+		actual,
+		newLocationInfo(io, srcPath),
+	}
+}
+
+// Actual is a getter of the actual value associated with the validation error.
+func (e ValidationExprError) Actual() interface{} { return e.actual }
+
+func (e ValidationExprError) Error() string {
+	return e.msgWithLocation(
+		validationFailedMsg(
+			fmt.Sprintf("not matching the expression, got %v", e.actual),
+		),
+	)
 }
--- kaitai/error_test.go
+++ kaitai/error_test.go
@@ -1,6 +1,11 @@
 package kaitai
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"reflect"
+	"testing"
+)
 
 func TestEndOfStreamError_Error(t *testing.T) {
 	tests := []struct {
@@ -35,19 +40,261 @@ func TestUndecidedEndiannessError_Error(t *testing.T) {
 		})
 	}
 }
-
-func TestValidationNotEqualError_Error(t *testing.T) {
+func Test_locationInfo_msgWithLocation(t *testing.T) {
+	type args struct {
+		msg string
+	}
 	tests := []struct {
-		name string
-		v    ValidationNotEqualError
-		want string
+		name      string
+		l         locationInfo
+		args      args
+		ioSeekPos int64
+		want      string
 	}{
-		{"Test Error", ValidationNotEqualError{}, "validation error"},
+		{
+			"msg", locationInfo{NewStream(bytes.NewReader([]byte("test"))), "/seq/0"}, args{"something failed"}, 2,
+			"/seq/0: at pos 2: something failed",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.v.Error(); got != tt.want {
-				t.Errorf("ValidationNotEqualError.Error() = %v, want %v", got, tt.want)
+			_, err := tt.l.io.Seek(tt.ioSeekPos, io.SeekStart)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			if got := tt.l.msgWithLocation(tt.args.msg); got != tt.want {
+				t.Errorf("locationInfo.msgWithLocation() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidationFailedError_interface(t *testing.T) {
+	io := NewStream(bytes.NewReader([]byte("test")))
+	actual := -1
+	srcPath := "types/header/seq/7"
+	tests := []struct {
+		name string
+		e    interface{}
+	}{
+		{"ValidationNotEqualError", NewValidationNotEqualError(2, actual, io, srcPath)},
+		{"ValidationLessThanError", NewValidationLessThanError(2, actual, io, srcPath)},
+		{"ValidationGreaterThanError", NewValidationGreaterThanError(2, actual, io, srcPath)},
+		{"ValidationNotAnyOfError", NewValidationNotAnyOfError(actual, io, srcPath)},
+		{"ValidationExprError", NewValidationExprError(actual, io, srcPath)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e, ok := tt.e.(ValidationFailedError)
+			if ok != true {
+				t.Fatalf("Type %T does not implement ValidationFailedError", tt.e)
+			}
+			if got := e.Actual(); got != actual {
+				t.Errorf("%T.Actual() = %v, want %v", e, got, actual)
+			}
+			if got := e.Io(); got != io {
+				t.Errorf("%T.Io() = %p, want %p", e, got, io)
+			}
+			if got := e.SrcPath(); got != srcPath {
+				t.Errorf("%T.SrcPath() = %q, want %q", e, got, srcPath)
+			}
+		})
+	}
+}
+
+func TestValidationNotEqualError_Error(t *testing.T) {
+	io := NewStream(bytes.NewReader([]byte("test")))
+	tests := []struct {
+		name string
+		e    ValidationNotEqualError
+		want string
+	}{
+		{
+			"integers", NewValidationNotEqualError(42, -1, io, "/seq/0"),
+			"/seq/0: at pos 0: validation failed: not equal, expected 42, but got -1",
+		},
+		{
+			"byte arrays", NewValidationNotEqualError([]uint8{160, 0}, []uint8{0, 160}, io, "/seq/1"),
+			"/seq/1: at pos 0: validation failed: not equal, expected [160 0], but got [0 160]",
+		},
+		{
+			"strings", NewValidationNotEqualError("ba", "ab", io, "/seq/2"),
+			"/seq/2: at pos 0: validation failed: not equal, expected ba, but got ab",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.e.Error(); got != tt.want {
+				t.Errorf("ValidationNotEqualError.Error() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidationNotEqualError_Expected(t *testing.T) {
+	io := NewStream(bytes.NewReader([]byte("test")))
+	tests := []struct {
+		name string
+		e    ValidationNotEqualError
+		want interface{}
+	}{
+		{"Expected", NewValidationNotEqualError(42, -1, io, "/seq/0"), 42},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.e.Expected(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ValidationNotEqualError.Expected() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidationLessThanError_Error(t *testing.T) {
+	io := NewStream(bytes.NewReader([]byte("test")))
+	tests := []struct {
+		name string
+		e    ValidationLessThanError
+		want string
+	}{
+		{
+			"integers", NewValidationLessThanError(42, -42, io, "/seq/0"),
+			"/seq/0: at pos 0: validation failed: not in range, min 42, but got -42",
+		},
+		{
+			"byte arrays", NewValidationLessThanError([]uint8{160, 0}, []uint8{0, 160}, io, "/seq/1"),
+			"/seq/1: at pos 0: validation failed: not in range, min [160 0], but got [0 160]",
+		},
+		{
+			"strings", NewValidationLessThanError("ba", "ab", io, "/seq/2"),
+			"/seq/2: at pos 0: validation failed: not in range, min ba, but got ab",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.e.Error(); got != tt.want {
+				t.Errorf("ValidationLessThanError.Error() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
+func TestValidationLessThanError_Min(t *testing.T) {
+	io := NewStream(bytes.NewReader([]byte("test")))
+	tests := []struct {
+		name string
+		e    ValidationLessThanError
+		want interface{}
+	}{
+		{"Min", NewValidationLessThanError(42, -1, io, "/seq/0"), 42},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.e.Min(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ValidationLessThanError.Min() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidationGreaterThanError_Error(t *testing.T) {
+	io := NewStream(bytes.NewReader([]byte("test")))
+	tests := []struct {
+		name string
+		e    ValidationGreaterThanError
+		want string
+	}{
+		{
+			"integers", NewValidationGreaterThanError(-42, 42, io, "/seq/0"),
+			"/seq/0: at pos 0: validation failed: not in range, max -42, but got 42",
+		},
+		{
+			"byte arrays", NewValidationGreaterThanError([]uint8{0, 160}, []uint8{160, 0}, io, "/seq/1"),
+			"/seq/1: at pos 0: validation failed: not in range, max [0 160], but got [160 0]",
+		},
+		{
+			"strings", NewValidationGreaterThanError("ab", "ba", io, "/seq/2"),
+			"/seq/2: at pos 0: validation failed: not in range, max ab, but got ba",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.e.Error(); got != tt.want {
+				t.Errorf("ValidationGreaterThanError.Error() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
+func TestValidationGreaterThanError_Max(t *testing.T) {
+	io := NewStream(bytes.NewReader([]byte("test")))
+	tests := []struct {
+		name string
+		e    ValidationGreaterThanError
+		want interface{}
+	}{
+		{"Max", NewValidationGreaterThanError(42, 45, io, "/seq/0"), 42},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.e.Max(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ValidationGreaterThanError.Max() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidationNotAnyOfError_Error(t *testing.T) {
+	io := NewStream(bytes.NewReader([]byte("test")))
+	tests := []struct {
+		name string
+		e    ValidationNotAnyOfError
+		want string
+	}{
+		{
+			"integer", NewValidationNotAnyOfError(-42, io, "/seq/0"),
+			"/seq/0: at pos 0: validation failed: not any of the list, got -42",
+		},
+		{
+			"byte array", NewValidationNotAnyOfError([]uint8{0, 160}, io, "/seq/1"),
+			"/seq/1: at pos 0: validation failed: not any of the list, got [0 160]",
+		},
+		{
+			"string", NewValidationNotAnyOfError("ab", io, "/seq/2"),
+			"/seq/2: at pos 0: validation failed: not any of the list, got ab",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.e.Error(); got != tt.want {
+				t.Errorf("ValidationNotAnyOfError.Error() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestValidationExprError_Error(t *testing.T) {
+	io := NewStream(bytes.NewReader([]byte("test")))
+	tests := []struct {
+		name string
+		e    ValidationExprError
+		want string
+	}{
+		{
+			"integer", NewValidationExprError(-42, io, "/seq/0"),
+			"/seq/0: at pos 0: validation failed: not matching the expression, got -42",
+		},
+		{
+			"byte array", NewValidationExprError([]uint8{0, 160}, io, "/seq/1"),
+			"/seq/1: at pos 0: validation failed: not matching the expression, got [0 160]",
+		},
+		{
+			"string", NewValidationExprError("ab", io, "/seq/2"),
+			"/seq/2: at pos 0: validation failed: not matching the expression, got ab",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.e.Error(); got != tt.want {
+				t.Errorf("ValidationExprError.Error() = %q, want %q", got, tt.want)
 			}
 		})
 	}
