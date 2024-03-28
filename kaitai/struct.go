19d66735188c46216b24d763949b0b81d3541e77
Mikhail Yakshin <greycat.na.kor@gmail.com>
2024-03-28 21:16:09 +0000

    Revert "Add kaitai.Struct: common interface providing Kaitai_IO() getter"
    This reverts commit 279452246e223aea13f0e734792fd12388a6f812.

--- kaitai/struct.go
+++
@@ -1,7 +0,0 @@
-package kaitai
-
-// Struct is the common interface guaranteed to be implemented by all types generated
-// by Kaitai Struct compiler.
-type Struct interface {
-	Kaitai_IO() *kaitai.Stream
-}
