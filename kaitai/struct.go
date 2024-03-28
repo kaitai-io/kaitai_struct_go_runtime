279452246e223aea13f0e734792fd12388a6f812
Mikhail Yakshin <greycat.na.kor@gmail.com>
2024-03-28 21:14:39 +0000

    Add kaitai.Struct: common interface providing Kaitai_IO() getter

---
+++ kaitai/struct.go
@@ -0,0 +1,7 @@
+package kaitai
+
+// Struct is the common interface guaranteed to be implemented by all types generated
+// by Kaitai Struct compiler.
+type Struct interface {
+	Kaitai_IO() *kaitai.Stream
+}
